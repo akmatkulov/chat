d72b45fc85e4209b182d4a6efd3587f95832afcd
dragon <pnz19620@gmail.com>
2023-10-15 17:45:25 +0600

    Add model like

--- db/migrate/20231014144705_create_rooms.rb
+++ db/migrate/20231014144705_create_rooms.rb
@@ -1,8 +1,8 @@
 class CreateRooms < ActiveRecord::Migration[7.1]
   def change
     create_table :rooms do |t|
-      t.belongs_to :user, null: :false, foreign_key: true 
-      t.string :title, null: :false, index: true
+      t.belongs_to :user, null: false, foreign_key: true 
+      t.string :title, null: false, index: true
       
       t.timestamps
     end
