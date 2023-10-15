9bd4be12f7fd6ebbe507c788b51f4b33f509a12c
dragon <pnz19620@gmail.com>
2023-10-15 20:54:58 +0600

    Add module like

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,24 @@
 class RoomsController < ApplicationController
     def index
+        @new_room = Room.new
+        @rooms = Room.all
     end
+
+    def show
+        @room = Room.find_by!(title: params[:title])
+        @messages = @room.messages
+        @new_message = current_user&.messages&.build
+    end
+
+    def create
+        @new_room = current_user&.rooms&.build(params_room)
+        if @new_room&.save
+            @new_room.broadcast_append_to :rooms
+        end
+
+    end
+    private 
+        def params_room
+            params.require(:room).permit(:title)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
+
+  resources :rooms, only: %i[show create], param: :title
+  resources :messages, only: %i[show create]
   root "rooms#index"
 end
